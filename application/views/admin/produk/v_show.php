29f1c4da0879572a63c0b494dd16e43965c971ad
tri <cobainaja1111@gmail.com>
2021-04-24 08:38:25 +0700

    update

---
+++ application/views/admin/produk/v_show.php
@@ -0,0 +1,64 @@
+<!-- Page Content-->
+<div class="page-content">
+        <div class="container-fluid">
+          <!-- Page-Title -->
+
+          <div class="row">
+            <div class="col-sm-12">
+              <h4>Beranda Menu</h4>
+            </div>
+          </div>
+          <!-- end page title end breadcrumb -->
+          <div class="row">
+            <div class="col-12">
+              <div class="card">
+                <div class="card-body">
+                  <h4 class="mt-0 header-title">Tabel Produk <button type="button" class="btn btn-primary float-right">Tambah Stok</button></h4>
+
+                  <!-- <p class="text-muted mb-3">DataTables has most features enabled by
+									default, so all you need to do to use it with your own tables is to call
+									the construction function: <code>$().DataTable();</code>.
+								</p> -->
+
+                  <table id="datatable" class="table table-bordered dt-responsive nowrap" style="border-collapse: collapse; border-spacing: 0; width: 100%">
+                    <thead>
+                      <tr>
+                        <th>Jenis Produk</th>
+                        <th>Nama Produk</th>
+                        <th>Harga</th>
+                        <th>Bahan</th>
+                        <th>Stok</th>
+                        <th>Catatan</th>
+                        <th>Aksi</th>
+                      </tr>
+                    </thead>
+
+                    <tbody>
+                      <tr>
+                        <td>sdad</td>
+                        <td>Tiger Nixon</td>
+                        <td>System Architect</td>
+                        <td>a</td>
+                        <td>s</td>
+                        <td>sa</td>
+                        <td>
+                          <div class="button-group">
+                            <button type="button" class="btn btn-primary" data-toggle="modal" data-animation="bounce" data-target=".edit-kontak">
+                              <i class="dripicons-pencil"></i>
+                            </button>
+                            <button type="button" class="btn btn-danger">
+                              <i class="dripicons-trash"></i>
+                            </button>
+                          </div>
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+            <!-- end col -->
+          </div>
+          <!-- end row -->
+        </div>
+        <!-- container -->
